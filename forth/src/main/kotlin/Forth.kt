1192e017607f8587c1982a895a619097ce95842b
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2024-10-01 15:40:06 +0200

    rename command to operation; add acc for commands

--- forth/src/main/kotlin/Forth.kt
+++ forth/src/main/kotlin/Forth.kt
@@ -1,39 +1,40 @@
 class Forth {
+    private data class ParsedLines(
+        val items: List<LineItem>,
+        val commands: Map<String, List<LineItem>>,
+    )
 
-    fun evaluate(
-        vararg line: String,
-    ): List<Int> {
-        return line[0] // TODO: make work for multi-line
-            .let(::parse)
-            .let(::evaluate)
-    }
+    fun evaluate(vararg line: String): List<Int> = line[0] // TODO: make work for multi-line
+        .let { parseLine(ParsedLines(listOf(), mapOf()), it) }
+        .items
+        .let(::evaluate)
 
-    fun parse(line: String): List<LineItem> = line
-        .lowercase()
-        .split("\\s+".toRegex())
-        .map(::parseWord)
+    private fun parseLine(acc: ParsedLines, line: String): ParsedLines =
+        line
+            .lowercase()
+            .split("\\s+".toRegex())
+            .map(::parseWord)
+            .let { ParsedLines(acc.items + it, acc.commands) }
 
     private fun parseWord(word: String): LineItem = word
         .toIntOrNull()?.let(LineItem::Number)
-        ?: LineItem.COMMANDS[word]
+        ?: LineItem.OPERATIONS[word]
         ?: throw Exception("undefined operation")
 
-    private tailrec fun evaluate(
-        lineItems: List<LineItem>,
-    ): List<Int> =
-        when (val firstCommandIndex = lineItems.indexOfFirst { it is LineItem.Command }) {
+    private tailrec fun evaluate(lineItems: List<LineItem>): List<Int> =
+        when (val firstOperationIndex = lineItems.indexOfFirst { it is LineItem.Operation }) {
             -1 -> lineItems.map { (it as LineItem.Number).inner }
             0 -> throw Exception("empty stack")
             else -> {
-                val firstCommand = lineItems[firstCommandIndex] as LineItem.Command
-                val firstArgIndex = firstCommandIndex - firstCommand.arity
+                val firstOperation = lineItems[firstOperationIndex] as LineItem.Operation
+                val firstArgIndex = firstOperationIndex - firstOperation.arity
                 if (firstArgIndex < 0) throw Exception("only one value on the stack")
                 val pre = lineItems.take(firstArgIndex)
                 val evaluated = lineItems
-                    .slice(firstArgIndex until firstCommandIndex)
+                    .slice(firstArgIndex until firstOperationIndex)
                     .map { it as LineItem.Number }
-                    .let(firstCommand::apply)
-                val post = lineItems.drop(firstCommandIndex + 1)
+                    .let(firstOperation::apply)
+                val post = lineItems.drop(firstOperationIndex + 1)
                 evaluate(pre + evaluated + post)
             }
         }
--- forth/src/main/kotlin/LineItem.kt
+++ forth/src/main/kotlin/LineItem.kt
@@ -1,7 +1,7 @@
 sealed class LineItem {
     class Number(val inner: Int) : LineItem()
 
-    class Command(val arity: Int, val f: (List<Int>) -> List<Int>) : LineItem() {
+    class Operation(val arity: Int, private val f: (List<Int>) -> List<Int>) : LineItem() {
         fun apply(arguments: List<Number>): List<Number> = arguments
             .map(Number::inner)
             .let(f)
@@ -9,20 +9,20 @@ sealed class LineItem {
     }
 
     companion object {
-        val COMMANDS = mapOf<String, Command>(
-            "+" to Command(2) { listOf(it[0] + it[1]) },
-            "-" to Command(2) { listOf(it[0] - it[1]) },
-            "*" to Command(2) { listOf(it[0] * it[1]) },
-            "/" to Command(2) {
+        val OPERATIONS = mapOf<String, Operation>(
+            "+" to Operation(2) { listOf(it[0] + it[1]) },
+            "-" to Operation(2) { listOf(it[0] - it[1]) },
+            "*" to Operation(2) { listOf(it[0] * it[1]) },
+            "/" to Operation(2) {
                 when (val b = it[1]) {
                     0 -> throw Exception("divide by zero")
                     else -> listOf(it[0] / b)
                 }
             },
-            "dup" to Command(1) { listOf(it[0], it[0]) },
-            "drop" to Command(1) { listOf() },
-            "swap" to Command(2) { listOf(it[1], it[0]) },
-            "over" to Command(2) { listOf(it[0], it[1], it[0]) },
+            "dup" to Operation(1) { listOf(it[0], it[0]) },
+            "drop" to Operation(1) { listOf() },
+            "swap" to Operation(2) { listOf(it[1], it[0]) },
+            "over" to Operation(2) { listOf(it[0], it[1], it[0]) },
         )
     }
 }
