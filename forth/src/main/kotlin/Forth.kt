1bdc59e3bf942ce6f3a8ca2f7414068be270088f
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2024-10-07 11:43:10 +0200

    rename

--- forth/src/main/kotlin/Forth.kt
+++ forth/src/main/kotlin/Forth.kt
@@ -1,5 +1,5 @@
 class Forth {
     fun evaluate(vararg line: String): List<Int> = line
-        .fold(ParsedLines(listOf(), mapOf())) { acc, it -> acc.parseLine(it) }
+        .fold(Parsed(listOf(), mapOf())) { acc, it -> acc.parseLine(it) }
         .let { Chunked(it.tokens).evaluate() }
 }
--- forth/src/main/kotlin/Parsed.kt
+++ forth/src/main/kotlin/Parsed.kt
@@ -1,26 +1,26 @@
-class ParsedLines(
+class Parsed(
     val tokens: List<Token>,
     private val commands: Map<String, List<Token>>,
 ) {
 
-    fun parseLine(line: String): ParsedLines = line
+    fun parseLine(line: String): Parsed = line
         .lowercase()
         .split("\\s+".toRegex())
         .let { parseWords(it) }
 
-    private fun parseWords(words: List<String>): ParsedLines =
+    private fun parseWords(words: List<String>): Parsed =
         when {
             words.firstOrNull() == ":" && words.lastOrNull() == ";" -> {
                 val command = words.drop(1).dropLast(1)
                 val key = command.firstOrNull()
                 val value = command.drop(1).flatMap { parseWord(it, commands) }
                 if (key == null || key.toIntOrNull() != null) throw Exception("illegal operation")
-                ParsedLines(tokens, commands + (key to value))
+                Parsed(tokens, commands + (key to value))
             }
 
             else -> words
                 .flatMap { parseWord(it, commands) }
-                .let { ParsedLines(tokens + it, commands) }
+                .let { Parsed(tokens + it, commands) }
         }
 
     private fun parseWord(word: String, commands: Map<String, List<Token>>): List<Token> = when (
