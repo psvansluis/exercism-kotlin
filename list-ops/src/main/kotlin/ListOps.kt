3ccb7e8746938fda320705954ef16a3879377bcf
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2024-10-11 11:03:21 +0200

    list-ops init

--- list-ops/src/main/kotlin/ListOps.kt
+++ list-ops/src/main/kotlin/ListOps.kt
@@ -1,29 +1,36 @@
-fun <T> List<T>.customAppend(list: List<T>): List<T> {
-    TODO("Implement this function to complete the task")
+fun <T> List<T>.customAppend(list: List<T>): List<T> = this + list
+
+fun List<Any>.customConcat(): List<Any> = when (val head = firstOrNull()) {
+    null -> this
+    else -> when (head) {
+        is List<*> -> (head as List<Any>).customConcat()
+        else -> listOf(head)
+    }
+        .customAppend(drop(1).customConcat())
 }
 
-fun List<Any>.customConcat(): List<Any> {
-    TODO("Implement this function to complete the task")
+fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> = customFoldLeft(listOf()) { acc, el ->
+    if (predicate(el)) acc + el else acc
 }
 
-fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> {
-    TODO("Implement this function to complete the task")
-}
+val List<Any>.customSize: Int
+    get() = when {
+        isEmpty() -> 0
+        else -> 1 + drop(1).customSize
+    }
 
-val List<Any>.customSize: Int get() = TODO("Implement this getter to complete the task")
+fun <T, U> List<T>.customMap(transform: (T) -> U): List<U> =
+    customFoldLeft(listOf()) { acc, el -> acc + transform(el) }
 
-fun <T, U> List<T>.customMap(transform: (T) -> U): List<U> {
-    TODO("Implement this function to complete the task")
-}
+fun <T, U> List<T>.customFoldLeft(initial: U, f: (U, T) -> U): U =
+    firstOrNull()
+        ?.let { drop(1).customFoldLeft(f(initial, it), f) }
+        ?: initial
 
-fun <T, U> List<T>.customFoldLeft(initial: U, f: (U, T) -> U): U {
-    TODO("Implement this function to complete the task")
-}
 
-fun <T, U> List<T>.customFoldRight(initial: U, f: (T, U) -> U): U {
-    TODO("Implement this function to complete the task")
-}
+fun <T, U> List<T>.customFoldRight(initial: U, f: (T, U) -> U): U =
+    lastOrNull()
+        ?.let { dropLast(1).customFoldRight(f(it, initial), f) }
+        ?: initial
 
-fun <T> List<T>.customReverse(): List<T> {
-    TODO("Implement this function to complete the task")
-}
+fun <T> List<T>.customReverse(): List<T> = customFoldRight(listOf()) { el, acc -> acc + el }
